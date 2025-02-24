ab7c59d9a56388e6fb7f71a06db75dff9a5685d2
Jasper Keith O. Vallecera <jasperkeith1.vallecera@gmail.com>
2025-02-24 19:41:16 +0800

    Added layout for recruiter routes

---
+++ src/app/recruiter/jobs/layout.tsx
@@ -0,0 +1,21 @@
+import { LogOutButton } from "~/app/dashboard/components/LogOutButton";
+import Image from "next/image";
+
+export default function RecruiterLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <>
+      <div className="bg-primary-300 pt-1"></div>
+      <div className="mx-auto max-w-7xl">
+        <header className="flex items-center justify-between pt-8">
+          <Image src={"/logo.png"} alt="Tukma Logo" className="h-12 w-auto" />
+          <div>
+            <LogOutButton />
+          </div>
+        </header>
+        {children}
+      </div>
+    </>
+  );
+}
